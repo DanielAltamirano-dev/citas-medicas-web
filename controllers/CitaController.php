6aa94f19d1fa59951af3529f5b449593a613f6e6
DanielMSI <daltamiran@bcmanabi.com>
2025-08-16 00:03:32 -0500

    Creacion del controlador con sus respectivos metodos

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -8,18 +8,18 @@ class CitaController {
         $this -> citaModel = new Cita();
     }
 
-    // Manejo de las solicitudes GET y POST
+    // Manejo de las solicitudes GET y POST, actua como un enrutador
     public function handleRequest(){
-        $method = $_SERVER('REQUEST_METHOD');
+        $method = $_SERVER['REQUEST_METHOD'];
 
         if($method == 'POST'){
             $action = $_POST['action'] ?? null;
 
             switch ($action) {
-                case 'crear';
+                case 'crear':
                     $this -> crearCita();
                     break;
-                case 'eliminar';
+                case 'eliminar':
                     $this -> eliminarCita();
                     break;
                 default:
@@ -32,5 +32,66 @@ class CitaController {
         }
     }
 
-    
+    // Crear una nueva cita
+    private function crearCita(){
+        $nombre = $_POST['nombre'] ?? null;
+        $especialidad = $_POST['especialidad'] ?? null;
+        $fecha = $_POST['fecha'] ?? null;
+
+        if(!nombre || !$especialidad || !$fecha) {
+            $this -> sendResponse(['error' => 'Faltan campos obligatorios'], 400);
+            return;
+        }
+
+        $resultado = $this -> citaModel -> crearCita($nombre, $especialidad, $fecha);
+
+        if($resultado) {
+            $this -> sendResponse(['mensaje: ' => 'Cita creada correctamente'], 201);
+        } else {
+            $this -> sendResponse(['mensaje error: ' => 'Error al crear la cita medica'], 500);
+        }
+
+    }
+
+    // Obtener todas las citas (filtradas por estado)
+    private function obtenerCitas(){
+        $citas = $this -> citaModel -> obtenerCitas();
+        
+        if($citas !== false){
+            $this -> sendResponse($citas, 200);
+        } else {
+            $this -> sendResponse(['mensaje error:' => 'Error al obtener las citas'], 500);
+        }
+    }
+
+    // Elimiado logico se una cita (SOFT DELETE)
+    private function eliminarCita(){
+        $id = $_POST['id'] ?? null;
+
+        if(!$id){
+            $this -> sendResponse(['mensaje error: ' => 'No se ha recibido el id'], 400);
+            return;
+        }
+
+        $resultado = $this -> citaModel -> eliminarCita($id);
+
+        if($resultado){
+            $this -> sendResponse(['mensaje: ' => 'Cita eliminada correctamente'], 200);
+        } else {
+            $this -> sendResponse(['mensaje error: ' => 'Error al eliminar la cita'], 500);
+        }
+
+    }
+
+    // Envia el mensaje de respuesta al usuario
+    private function sendRespone($data, $statusCode = 200){
+        http_response_code($statusCode);
+        header('Content-Type: application/json');
+        echo json_encode($data);
+        exit;
+    }
 }
+
+// Se instancia el controlador y maneja la solicitud
+$controller = new CitaController();
+$controller -> handleRequest();
